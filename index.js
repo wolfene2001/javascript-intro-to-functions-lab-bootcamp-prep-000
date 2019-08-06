2b2a66f8112b9fd973f249d6d5385ef38d0b3992
wolfene2001 <wolfene2001@yahoo.com>
2019-08-06 20:59:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   
-  if  (string.toLowercase === string)
+  if  (string.toLowerCase() === string)
   {
     return "I can't hear you!"
-  } else if(string.toUppercase === string) {
+  } else if(string.toUpperCase() === string) {
     return "YES INDEED!"
   } else if(string === "I love you,Grandma.") {
     return "I love you too."
